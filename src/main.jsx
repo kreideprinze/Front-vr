0a60d5403245287365607946229906a7b347174f
Darsh Kuchhal <d3kuchhal@gmail.com>
2026-01-05 23:03:30 +0530

    update-3-gh

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/Front-DK">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
