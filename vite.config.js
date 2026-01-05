5865c3d25082d793fd48f18c1863ee9743fb81bf
Darsh Kuchhal <d3kuchhal@gmail.com>
2026-01-05 22:46:51 +0530

    updated-for-gh

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/react-vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Front-DK/', // This must match your GitHub repository name exactly
 })
